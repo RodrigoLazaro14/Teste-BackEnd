3e021e786ed8803d79bac5f679ed3142ce12ad0c
Rodrigo Lázaro <70705052+RodrigoLazaro14@users.noreply.github.com>
2021-12-19 18:48:37 -0300

    teste

--- calculo-rentabilidade/src/main/java/com/testebackend/calculorentabilidade/CalculoRentabilidadeApplication.java
+++ calculo-rentabilidade/src/main/java/com/testebackend/calculorentabilidade/CalculoRentabilidadeApplication.java
@@ -8,6 +8,7 @@ public class CalculoRentabilidadeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculoRentabilidadeApplication.class, args);
+		boolean teste;
 	}
 
 }
