19693515a168d8b011809df97133514d2a0ea0fb
Rodrigo Lázaro <70705052+RodrigoLazaro14@users.noreply.github.com>
2021-12-19 18:22:54 -0300

    Projeto inicial

---
+++ calculo-rentabilidade/src/main/java/com/testebackend/calculorentabilidade/CalculoRentabilidadeApplication.java
@@ -0,0 +1,13 @@
+package com.testebackend.calculorentabilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoRentabilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoRentabilidadeApplication.class, args);
+	}
+
+}
